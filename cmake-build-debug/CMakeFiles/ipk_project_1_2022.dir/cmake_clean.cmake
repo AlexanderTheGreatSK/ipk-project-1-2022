7e96be544c0946c9c3b5c5c95d317a2df2f5f722
AlexanderTheGreatSK <alex.okrucky@gmail.com>
2022-02-10 20:05:28 +0100

    init commit

---
+++ cmake-build-debug/CMakeFiles/ipk_project_1_2022.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ipk_project_1_2022.dir/main.c.o"
+  "ipk_project_1_2022"
+  "ipk_project_1_2022.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ipk_project_1_2022.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ipk_project_1_2022.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ipk_project_1_2022.
+# This may be replaced when dependencies are built.
